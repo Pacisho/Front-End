bcb0b6c34b1e4262cc85329e89d6b037d1b20da4
Pacisho <phatdanaikaeochan@gmail.com>
2025-08-04 12:25:29 +0700

    Create page.js

---
+++ app/admin/users/page.js
@@ -0,0 +1,79 @@
+'use client';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const res = await fetch('http://itdev.cmtc.ac.th:3000/api/users');
+        if (!res.ok) {
+          console.error('Failed to fetch data');
+          return;
+        }
+        const data = await res.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    getUsers();
+    const interval = setInterval(getUsers, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <br /><br /><br /><br />
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            Users List
+          </div>
+          <div className="card-body">
+            <div className="row">
+              <table className="table table-striped table-hover">
+                <thead>
+                  <tr>
+                    <th className='col-md-1 text-center'>#</th>
+                    <th className='col-md-2'>Firstname</th>
+                    <th className='col-md-2'>Fullname</th>
+                    <th className='col-md-2'>Lastname</th>
+                    <th className='col-md-2'>Username</th>
+                    <th className='col-md-2'>Password</th>
+                    <th className='col-md-3'>Address</th>
+                    <th className='col-md-1'>Sex</th>
+                    <th className='col-md-2'>Birthday</th>
+                    <th className='col-md-1'>Edit</th>
+                    <th className='col-md-1'>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item) => (
+                    <tr key={item.id}>
+                      <td className='text-center'>{item.id}</td>
+                      <td>{item.firstname}</td>
+                      <td>{item.fullname}</td>
+                      <td>{item.lastname}</td>
+                      <td>{item.username}</td>
+                      <td>{item.password}</td>
+                      <td>{item.address}</td>
+                      <td>{item.gender}</td>
+                      <td>{item.birthDate}</td>
+                      <td><Link href="/admin/users/edit/[id]" as={`/admin/users/edit/${item.id}`} className="btn btn-warning">Edit</Link></td>
+                      <td><button className="btn btn-pill btn-danger" type="button"><i className="fa fa-trash"></i> Del</button></td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br /><br />
+    </>
+  );
+}
